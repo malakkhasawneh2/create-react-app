17cb07e7c1d8d6bc39fa615506fc5219199c3585
malakkhasawneh2 <malakkhasawneh2@gmail.com>
2021-08-29 23:22:16 +0300

    react-app

---
+++ horned-beasts/src/App.js
@@ -0,0 +1,27 @@
+import React  from 'react';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
+
+class App extends React.Component{
+render(){
+return( 
+<>
+<Header/>
+<Main/>
+<Footer/>
+</>
+
+ )
+
+}
+
+
+}
+export default App;
+
+
+
+
+
+
---
+++ horned-beasts/src/component/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class Footer extends React.Component{
+render(){
+    return(<h3>&copy; malakkhasawneh2021</h3>)
+}
+}
+export default Footer
