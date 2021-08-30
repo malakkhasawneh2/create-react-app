2480eedc354455c382383ba3e981411b14e84647
malakkhasawneh2 <malakkhasawneh2@gmail.com>
2021-08-30 15:00:36 +0300

    lap 2

--- horned-beasts/src/component/HornedBeasts.js
+++ horned-beasts/src/component/HornedBeasts.js
@@ -1,12 +1,44 @@
 import React from 'react'
-class HornedBeasts extends React.Component{
-render()
-{
-    return(<>
-    <h2>{this.props.title}</h2>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+
+
+class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfPets : 0
+        }
+    }
+
+    incrementNumberOfPets = () => {
+        this.setState({
+            numberOfPets : this.state.numberOfPets + 1  
+        })
+    }
+    render() {
+        return (
+            <>
+                {/* <h2>{this.props.title}</h2>
     <img src={this.props.imageUrl}/>
-    <p>{this.props.description}</p>
-    </>)
-}
+    <p>{this.props.description}</p> */}
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.imageUrl} onClick={this.incrementNumberOfPets}/>
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                        {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                            Number of pets 🐾: {this.state.numberOfPets}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </>
+        )
+    }
 }
 export default HornedBeasts
